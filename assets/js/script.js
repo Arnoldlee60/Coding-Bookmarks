e85a312ecb455bc4dbb4c9d3772a05416759d224
Arnold Lee <arnoldlee60@gmail.com>
2021-06-10 02:48:23 -0500

    functions working with button and input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,9 @@
-fetch('http://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow', 
-    {
-      method: 'GET', //GET is the default.
-      credentials: 'same-origin', // include, *same-origin, omit
-      redirect: 'follow', // manual, *follow, error
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //console.log(data);
-        for(var i = 0 ; i < data.items.length; i++)
-        {
-        //console.log(data.items[i].name);
-        }
-      }); //get all the popular tags from stackoverflow
-      
-function linkPreviewCreation(stacklink){
-    fetch(stacklink, 
-    {
-      method: 'GET', //GET is the default.
-      credentials: 'same-origin', // include, *same-origin, omit
-      redirect: 'follow', // manual, *follow, error
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-
-        //console.log(data);
-        console.log(data.url)
-        console.log(data.description)
-      }); //link preview creator 
-    }
 function search(){ //(description, tag) input and dropdown use onclick() to search on submit button
-    var tag = 'javascript'; //change to dropdown menu value
-    var description = 'Uncaught TypeError' //change to input value
-    var createLink =  'http://api.stackexchange.com/2.2/search?order=desc&sort=relevance&tagged=' + tag + '&intitle=' + description + '&site=stackoverflow'
+  var inputtedSearch = document.querySelector('#input').value;
+  var inputtedTag = document.querySelector('#tags').value;
+    //var tag = 'javascript'; //change to dropdown menu value
+    //var description = 'Uncaught TypeError' //change to input value
+    var createLink =  'http://api.stackexchange.com/2.2/search?order=desc&sort=relevance&tagged=' + inputtedSearch + '&intitle=' + inputtedTag + '&site=stackoverflow'
     fetch(createLink, 
     {
       method: 'GET', //GET is the default.
@@ -54,6 +22,7 @@ function search(){ //(description, tag) input and dropdown use onclick() to sear
         else{
           for(var i = 0; i < 5; i++) //data.items.length
           {
+            console.log(data);
             //console.log(data.items[i].link)
             //var stacklink = 'http://api.linkpreview.net/?key=6183f2f21f3a5da93aa0c053ff2a7356 &q=' + data.items[i].link;
             //console.log("Link Preview Starts here ")
@@ -64,11 +33,53 @@ function search(){ //(description, tag) input and dropdown use onclick() to sear
       }); 
     }
 
+    function linkPreviewCreation(stacklink){
+      fetch(stacklink, 
+      {
+        method: 'GET', //GET is the default.
+        credentials: 'same-origin', // include, *same-origin, omit
+        redirect: 'follow', // manual, *follow, error
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+  
+          //console.log(data);
+          console.log(data.url)
+          console.log(data.description)
+        }); //link preview creator 
+      }
 
       var url = "https://www.youtube.com/watch?v=jNQXAC9IVRw"; //placeholder
 function createYouTubeEmbedLink (url) {
     return url.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
     }
+
+
+    fetch('http://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow', 
+    {
+      method: 'GET', //GET is the default.
+      credentials: 'same-origin', // include, *same-origin, omit
+      redirect: 'follow', // manual, *follow, error
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        //console.log(data);
+        for(var i = 0 ; i < data.items.length; i++) //data.items.length
+        {
+        //console.log(data.items[i].name);
+        var x = document.createElement("OPTION");
+        x.setAttribute("id", "tags", data.items[i].name);
+        var t = document.createTextNode(data.items[i].name);
+        x.appendChild(t);
+        document.getElementById("menuOptions").appendChild(x);
+        }
+      }); //get all the popular tags from stackoverflow
+
+
 
 /*
 todo:
