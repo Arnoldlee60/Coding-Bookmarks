3491f94d003e60c68e86610dbe2d34c6d08312ad
Ricardo Salazar <no-replay>
2021-06-10 19:02:28 -0500

    add html elements

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-fetch('http://api.linkpreview.net/?key=123456&q=https://www.google.com', 
-    {
-      method: 'GET', //GET is the default.
-      credentials: 'same-origin', // include, *same-origin, omit
-      redirect: 'follow', // manual, *follow, error
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data.description);
-      });
+
+fetch('http://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow',
+  {
+    method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (var i = 0; i < data.items.length; i++) {
+      console.log(data.items[i].name);
+    }
+  }); //tags 
+ 
